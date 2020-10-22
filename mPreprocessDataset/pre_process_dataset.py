a1719712849157d3d958f96abab11e566a0d0679
khizarziz <khizer.awan@gmail.com>
2020-10-22 23:45:42 +0000

    image crop_tranform almost completed

--- mPreprocessDataset/pre_process_dataset.py
+++ mPreprocessDataset/pre_process_dataset.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.io import loadmat
 from pathlib import Path
 from datetime import datetime
-from mtcnn import MTCNN
+import dlib
 
 
 # load dataset
@@ -13,7 +13,9 @@ from mtcnn import MTCNN
 # save dataset as pandas,feather & imencode for size efficiency
 
 Dataset_DF = pd.DataFrame(columns=["age", "gender", "image", "org_box", "trible_box", "landmarks", "roll", "yaw", "pitch"])
-detector = MTCNN()
+#initiate face detector and predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 class Process_WIKI_IMDB():
   
@@ -43,59 +45,49 @@ class Process_WIKI_IMDB():
     # save dataframe as csv
     Dataset_DF.to_csv(self.base_path.joinpath(self.dataset_name+'.csv'),index=False)
 
-  def detect_face_box_lm(self,image):
-    facejson = detector.detect_faces(image)
-
-    if len(facejson) > 1:
-      # more than 1 face... do something
-      return None
-    
-    for current_face_json in facejson:
-      bounds = json['box']
-      keypoints = json['keypoints']
-
-    bounds, lmarks = ret
-    if only_one and len(bounds) > 1:
-        print("!!!!!,", bounds, lmarks)
-        raise Exception("more than one face %s"%image_path)
-    return ret
+  def detect_face_and_landmarks(self,image):
+    face_rect_list = detector.detect(image)
+    # make a landmarks_list of all faces detected in image
+    lmarks_list = [predictor(image,face_rect) for face_rect in face_rect_list] # getting landmarks as matrix
+    # lmarks_matrix = np.matrix([[p.x,p.y] for p in predictor(image,face_rect[0]).parts()])
+    return face_rect_list, lmarks_list
 
   def crop_and_transform_images(self):
     meta_dataframe = pd.read_csv(self.base_path.joinpath(self.dataset_name+'.csv')
-    
     #filter out where second face != null (image have 2 faces)
     meta_dataframe = meta_dataframe[meta_dataframe.second_face_score.isna()]
-
     for index,series in meta_dataframe.iterrow():
       # clear multiple faces
       image_path = series.full_path
       try:
           image = cv2.imread(image_path, cv2.IMREAD_COLOR)
           image = cv2.copyMakeBorder(image, self.extra_padding, self.extra_padding, self.extra_padding, self.extra_padding, cv2.BORDER_CONSTANT)
-
-          bounds, lmarks = gen_face(detector, image, image_path)
-          crops = detector.extract_image_chips(image, lmarks, padding=0.4)  # aligned face with padding 0.4 in papper
-          if len(crops) == 0:
-              print("cant detect!!!!")
-              raise Exception("no crops~~ %s---%s"%(image_path, series.age))
-          if len(crops) > 1:
-              raise Exception("more than one face~---%s~-- %s"%(series.name, image_path))
-          bounds, lmarks = gen_face(detector, crops[0], image_path)  # recaculate landmar
-          org_box, first_lmarks = bounds[0], lmarks[0]
-          trible_box = gen_boundbox(org_box, first_lmarks)
+          face_rect_list, lmarks_list = detect_face_and_landmarks(image) # Detect face & landmarks
+          if len(face_rect_list) > 1:
+            do something here to track 2 faced image... later skip it somehow
+            continue
+          elif len(face_rect_list) > 1:
+            skip image too, because no face detected
+            continue
+          #extract_image_chips will crop faces from image according to size & padding and align them in upright position and return list of them
+          cropped_faces = detector.extract_image_chips(image, lmarks_list,size=64 padding=0.4)  # aligned face with padding 0.4 in papper
+          # detect face landmarks again from cropped & align face.  (as positions of lmarks are changed in cropped image)
+          face_rect_list, lmarks_list = detect_face_and_landmarks(cropped_faces[0]) # Detect face from cropped image
+          face_rect, first_lmarks = face_rect_list[0], lmarks_list[0] # getting first face's rectangle box and landmarks 
+          trible_box = gen_boundbox(face_rect, first_lmarks)
           pitch, yaw, roll = get_rotation_angle(crops[0], first_lmarks) # gen face rotation for filtering
           image = crops[0]   # select the first align face and replace
       except Exception as ee:
           logging.info("exception as ee: %s"%ee)
           print(ee)
           trible_box = np.array([])
-          org_box, first_lmarks = np.array([]), np.array([])
+          face_rect, first_lmarks = np.array([]), np.array([])
           pitch, yaw, roll = np.nan, np.nan, np.nan
           age = np.nan
           gender = np.nan
       status, buf = cv2.imencode(".jpg", image)
       series["image"] = buf.tostring() 
-      series["org_box"] = org_box.dumps()  # xmin, ymin, xmax, ymax
+      series["face_rect"] = face_rect.dumps()  # xmin, ymin, xmax, ymax
       series["landmarks"] = first_lmarks.dumps()  # y1..y5, x1..x5
       series["trible_box"] = trible_box.dumps() 
       series["yaw"] = yaw
